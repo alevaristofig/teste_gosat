3f16efb8f7ead0b50b66f4b96797d5b6aecb75bd
Alexandre <alevaristofig@gmail.com>
2025-05-21 00:24:13 -0300

    Modificação nas rotas

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CreditoController;
 use App\Http\Controllers\Api\UserController;
 
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
-
 Route::prefix('v1/simulacao')->group(function() {
 
     Route::post('/login',[UserController::class,'login']);
