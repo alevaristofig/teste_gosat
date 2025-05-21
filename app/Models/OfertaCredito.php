5b5722e23416402052e449dc117967bfacec8852
Alexandre <alevaristofig@gmail.com>
2025-05-21 11:07:51 -0300

    Correção de erros

--- app/Models/OfertaCredito.php
+++ app/Models/OfertaCredito.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfertaCredito extends Model
 {
+    protected $table = "oferta_credito";
+
     protected $fillable = [
         'id_instiuicao', 'cpf', 'instituicao_financeira', 'modalidade_credito', 'valor_a_pagar',
         'valor_solicitado', 'taxa_juros', 'qnt_parcelas'
--- app/Service/CreditoService.php
+++ app/Service/CreditoService.php
@@ -6,6 +6,7 @@
     use Illuminate\Http\JsonResponse;
     use Illuminate\Http\Request;
     use App\Models\OfertaCredito;
+    use App\Exceptions\ApiMessages;
 
     class CreditoService {
 
